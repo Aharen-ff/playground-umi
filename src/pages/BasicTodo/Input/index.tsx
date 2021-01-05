2d88e824ada14c2ea0483ddbfbf04afed267e8ba
Liam <liam.ch.guo@outlook.com>
2021-01-05 13:38:40 +0800

    add some comments

--- src/pages/BasicTodo/Input/index.tsx
+++ src/pages/BasicTodo/Input/index.tsx
@@ -12,18 +12,25 @@ const TodoInput: FC<IProps> = ({ addTodo, todoList }): ReactElement => {
   const placeholder = 'Enter your Todo item';
   const inputRef = useRef<Input>(null);
 
+  /**
+   * @func handleInputValueChange
+   * @desc There is no two-way data bindings in React,
+   * so we need to handle each input value change event,
+   * and update state manually.
+   * @param {React.ChangeEvent} e
+   */
   const handleChange = (e: React.ChangeEvent): void => {
     /**
      * I've fixed this problem. I think it's because
      * TypeScript cannot recognize what `target` is,
-     * which need a explicit declaration.
+     * which need a explicit declaration `as HTMLInputElement`.
      */
     const value: string = (e.target as HTMLInputElement).value;
     setValue(value);
   };
 
   const addItem = (): void => {
-    const value: string = inputRef.current!.state.value.trim();
+    const value: string = inputValue;
 
     if (value.length) {
       const isExist = todoList.find(todo => todo.content === value);
